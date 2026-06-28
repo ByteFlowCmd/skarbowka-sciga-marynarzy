44437775814ab7a6011d96b7369af352d7726b29
Robert <135177567+ByteFlowCmd@users.noreply.github.com>
2026-06-28 19:29:57 -0300

    Add files via upload

---
+++ Copy_files/embed.php
@@ -0,0 +1,49 @@
+var _____WB$wombat$assign$function_____=function(name){return (globalThis._wb_wombat && globalThis._wb_wombat.local_init && globalThis._wb_wombat.local_init(name))||globalThis[name];};if(!globalThis.__WB_pmw){globalThis.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opener = _____WB$wombat$assign$function_____("opener");
+  if (typeof(window.ONTVdebug)=="undefined"){window.ONTVdebug = function( a , l ){ if (l<=0){ console.log("[embed]["+(Date.now())+"ms]");console.log( a ); }}}
+  window.ONTVdebug("Onnetwork Player Loader",1);
+      window.ONTVdebug( "SID websiteID:564 " , 0.5 );
+    window.ONTVdebug( "SID videoID:0 " , 0.5 );
+    window.ONTVdebug( "SID playlistID:5498 " , 0.5 );
+var _ONND_URL_EMBED = 'https://web.archive.org/web/20260628030451/https://video.onnetwork.tv' ;
+var _ONND_URL_CDN_EMBED = 'https://web.archive.org/web/20260628030451/https://cdn.onnetwork.tv' ;
+var _ONNPBaseId = 'ONNPc1219abe27039589acfad14e9df8d9a3' ;
+  try{(new Image(1,1)).src=_ONND_URL_CDN_EMBED + "/cnt/a_cntg.png?ts="+Date.now()+"&d=564&wsc=00&typ=embed&mobile=0&c=43";}catch(q){}
+(function(){
+  var cs = document.currentScript ;
+  import('https://web.archive.org/web/20260628030451/https://cdn.onnetwork.tv/js/player90/embed.min.js?s=1782388321').then((Module) => {
+    var config={"_detweb":0,"version":"86","mobile":0,"deviceClass":"desktop","ios":0,"safari":0,"wid":564,"csweb":0,"partnerId":552,"widget":0,"cid":0,"lang":0,"eb":0,"containerId":null,"cpcId":null,"mid":"MCwxNng5LDQsMiw1NDk4LDU2NCwwLDAsMiwxMCwwLDAsMywwLDEsMSwwLDEsMSwwLDAsMCwwLDAsMCw1LDEsNDA7LTE7LTE7NDA7Qkw7NDA7MDs0MDsyNCwxLDYwLDExLDAsMCwwOzA7MDswOzA7MDswLDE=","ar":56.25,"ar1":"16 / 9","ars":"16 / 9","preview":0,"iid":1123310690,"usf":1,"playerDelay":1,"playerDDelay":100,"playerType":0,"par":"0,16x9,4,2,5498,564,0,0,2,10,0,0,3,0,1,1,0,1,1,0,0,0,0,0,0,5,1,40;-1;-1;40;BL;40;0;40;24,1,60,11,0,0,0;0;0;0;0;0;0,1","stickyReqAd":"","stickyMode":3,"stickyPos":"BL","stickyHMargin":40,"stickyVTMargin":0,"stickyVBMargin":40,"stickyLevel":0.6,"stickyCloser":1,"stickyCloserDelay":5,"stickyBodyExcludeClasses":[""],"stickyIgnoreOther":0,"stickyWithFirstMovieOnly":0,"stickyRestoreWithAd":0,"stickyRestoreWithMovie":1,"stickyRestoreWithNext":1,"stickyRestoreWithScroll":0,"stickyMode1WithStart":0,"stickyMode2From6WithStart":0,"stickyBlockAdViewable":0,"stickyBlockClosedAdViewable":0,"stickyTarget":"","stickyHideIfViewable":0,"stickyHidePOP":0,"nsich":0,"stickyAlways":0,"stickyOnly":0,"scheduledSticky":0,"stickyFailover":0,"pvm":0,"pva":0,"pplcmt":0,"btvpnaa":0,"outstream":"","outstreamCloser":0,"passkeys":[],"showHidden":0,"unHide":0,"abm":0,"ddagora":0,"mtpromoplay":0,"sft":0,"ap":0,"sfs":0,"map":-1,"mtitle":"","exmp4":"","exhls":"","exth":"","exdur":0,"vids":"","cc":"","mapp":"","ampsrc":0,"podcast":0,"gptslot":"","gptsizes":"[]","gptcompanionslot":"","gptcompanionsizes":null,"gptcompanionheight":0,"gptcompanionheightinc":0,"gptcompanionrefresh":30,"gptrefresheradtext":"REKLAMA","gptcompanionadtext":"REKLAMA","gptfillertext":"NAST\u0118PNE MATERIA\u0141Y","gptrefresherslot":"","gptrefreshersizes":[],"gptrefresherheight":0,"gptrefresherheightinc":0,"gptrefresherrefresh":30,"passbackoutstreamwid":0,"passbackoutstream":0,"pdis":0,"cdis":0,"cfiller":0,"gpna":0,"gpidle":0,"gpabc":0,"gphai":0,"atmla":0,"wtgevent":"","wtgeventstay":0,"isCpl":0,"customPlayer":null,"vasturl":"","vastUrl2":"","srq":0,"svm":0,"eventAutoplayStart":0,"eventStickyLevel":0,"eventStickyPos":"","potePos":"","useDfpParams":0,"useDfpOrg":0,"useDfpParamsAudio":0,"prismhit":"","prismid":"","debComs":5.1,"debPlComs":5.1,"debAdComs":5.1,"debMvComs":5.1,"debTimings":5.1,"debSticky":5.1,"debCPEvent":5.1,"debScheduler":5.1,"debAutovideo":5.1,"maxT2SLength":0,"zIndex":"","minWidth":"","maxWidth":"","minHeight":"","maxHeight":"","margin":"","marginSticky":"","paddingSticky":"","maxWidthSticky":"","maxHeightSticky":"","borderSticky":"","zIndexSticky":"","closerWidth":"","closerHeight":"","closerContent":"","closerBackground":"","closerTopMargin":"","bbarHeight":"","bbar":"","headerHeight":"","headerContent":"","muteicon":"","frameTitle":"","frameBorder":"","transition":"","RHACe":0,"RHAC":{"selector":null,"addAfterglow":0,"textInfo":null,"afterAds":0,"afterAdBlock":0,"ifAdNotPresent":0,"afterVideo":0,"afterVideoTime":0,"ifAdBlocked":0,"ifSkipped":0},"viewStartLevel":10,"customModule":"https://web.archive.org/web/20260628030451/https://cdn.onnetwork.tv/js/player90/embedCustomPlayer.min.js?s=1782388321","useurldata":0,"urldatatags":0,"urldatadatealgo":0,"urldatadateselector":null,"urldatadateselector2":null,"urldatadatedistance":0,"tagclass":"","tagsAlgo":0,"tagsClass":null,"uvpap":0,"uvpapfb":0,"dfs":0} ;
+    var player = new Module.ONNPlayerClass( config , cs );
+  });
+})();
+
+}
+
+/*
+     FILE ARCHIVED ON 03:04:51 Jun 28, 2026 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:12:15 Jun 28, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  capture_cache.get: 1.652
+  captures_list: 0.513
+  exclusion.robots: 0.053
+  exclusion.robots.policy: 0.042
+  esindex: 0.007
+  cdx.remote: 88.315
+  LoadShardBlock: 260.948 (3)
+  PetaboxLoader3.datanode: 111.54 (4)
+  PetaboxLoader3.resolve: 170.264 (3)
+  load_resource: 119.581
+*/
